10dfe8d4f0ef4f4ed38be7a8cd37883e78c10286
faith pueneh <puenehfaith@gmail.com>
2020-11-28 19:07:19 +0100

    updated

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -3,9 +3,8 @@
 function darkMode(){
     let mode = document.getElementById("grid");
     mode.classList.toggle("light-mode");
+    
 
-    let secondPart = document.getElementById("six");
-    secondPart.classList.toggle("sixcolor");
     
 }
 darkMode();
