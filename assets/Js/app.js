a9388b38bcd3fada9c8ad3629f22907088362d79
faith pueneh <puenehfaith@gmail.com>
2020-12-01 07:31:53 +0100

    updated

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -2,9 +2,9 @@
 
 function darkMode(){
     let mode = document.getElementById("grid");
-    mode.classList.toggle("light-mode");
-    
-
+    mode.classList.toggle("light-mode"); 
     
 }
+
+
 darkMode();
