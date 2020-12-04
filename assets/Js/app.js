d8987d1c6127da5ae7054e37d063fd98e85d7be8
faith pueneh <puenehfaith@gmail.com>
2020-12-04 11:06:56 +0100

    updated

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -2,7 +2,11 @@
 
 function darkMode(){
     let mode = document.getElementById("grid");
+    let b = document.getElementById("cardcolor");
+    let c = document.getElementById("thecolor");
     mode.classList.toggle("light-mode"); 
+    b.classList.toggle("whitecard");
+    c.classList.toggle("whitecard");
     
 }
 
